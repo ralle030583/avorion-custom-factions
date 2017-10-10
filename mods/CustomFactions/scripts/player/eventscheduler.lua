faf668cf5e3683d94d0b949f497b7dd9dffa4af1
Ralle030583 <ralle030583@users.noreply.github.com>
2017-10-10 20:54:22 +0200

    syntax error

--- mods/CustomFactions/scripts/player/eventscheduler.lua
+++ mods/CustomFactions/scripts/player/eventscheduler.lua
@@ -7,7 +7,7 @@ function EventSheduler.addCustomEvents(events)
     events.insert({schedule = random():getInt(25, 50) * 60, script = "mods/CustomFactions/scripts/player/khillmarrattack", arguments = {0}, minimum = 5 * 60, from = 0, to = 500})
     events.insert({schedule = random():getInt(35, 70) * 60, script = "mods/CustomFactions/scripts/player/khillmarrattack", arguments = {1}, minimum = 25 * 60, to = 500})
     events.insert({schedule = random():getInt(60, 80) * 60, script = "mods/CustomFactions/scripts/player/khillmarrattack", arguments = {2}, minimum = 60 * 60, to = 350})
-    events.insert({schedule = random():getInt(80, 120) * 60, script = mods/CustomFactions/scripts/player/khillmarrattack", arguments = {3}, minimum = 120 * 60, to = 300})
+    events.insert({schedule = random():getInt(80, 120) * 60, script = "mods/CustomFactions/scripts/player/khillmarrattack", arguments = {3}, minimum = 120 * 60, to = 300})
   end
   return events
 end
