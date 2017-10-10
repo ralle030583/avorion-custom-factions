c4d1a667418eb96a11768177e75ba47fe4655eee
Ralle030583 <ralle030583@users.noreply.github.com>
2017-10-10 22:17:19 +0200

    CLASS.MILITARY.FRIGATTE => CLASS.MILITARY.FRIGATE

--- mods/CustomFactions/config/CustomFactionsConfig.lua
+++ mods/CustomFactions/config/CustomFactionsConfig.lua
@@ -8,7 +8,7 @@ CustomFactionsConfig.pauseTime = 10 -- Pausetime after events in minutes
 CustomFactionsConfig.CLASS.MILITARY.SCOUT = 0
 CustomFactionsConfig.CLASS.MILITARY.SENTINEL = 1
 CustomFactionsConfig.CLASS.MILITARY.HUNTER = 2
-CustomFactionsConfig.CLASS.MILITARY.FRIGATTE = 3
+CustomFactionsConfig.CLASS.MILITARY.FRIGATE = 3
 CustomFactionsConfig.CLASS.MILITARY.CRUISER = 4
 CustomFactionsConfig.CLASS.MILITARY.DESTROYER = 5
 CustomFactionsConfig.CLASS.MILITARY.DREADNOUGHT = 6
@@ -25,17 +25,17 @@ CustomFactionsConfig.ships["Khillmarr"][CustomFactionsConfig.CLASS.MILITARY.SENT
   = {{title = "Dagger Corvette" , plan = "mods/CustomFactions/plans/khillmarr/KhillmarrDaggerCorvette_1.xml", scale = 1.0}}
 CustomFactionsConfig.ships["Khillmarr"][CustomFactionsConfig.CLASS.MILITARY.HUNTER]
   = {{title = "Light Destroyer" , plan = "mods/CustomFactions/plans/khillmarr/KhillmarrLightDestroyer_2.xml", scale = 1.0}}
-CustomFactionsConfig.ships["Khillmarr"][CustomFactionsConfig.CLASS.MILITARY.FRIGATTE]
+CustomFactionsConfig.ships["Khillmarr"][CustomFactionsConfig.CLASS.MILITARY.FRIGATE]
   = {{title = "Light Cruiser" , plan = "mods/CustomFactions/plans/khillmarr/KhillmarrLightCruiserPride_3.xml", scale = 1.0}}
 CustomFactionsConfig.ships["Khillmarr"][CustomFactionsConfig.CLASS.MILITARY.CRUISER]
   = {{title = "Light Cruiser MK II" , plan = "mods/CustomFactions/plans/khillmarr/KhillmarrLightCruiserMK2_4_5.xml", scale = 1.0}}
 CustomFactionsConfig.ships["Khillmarr"][CustomFactionsConfig.CLASS.MILITARY.DESTROYER]
   = {{title = "Cruiser MK II" , plan = "mods/CustomFactions/plans/khillmarr/KhillmarrLightCruiserMK2_4_5.xml", scale = 1.5}}
 
-CustomFactionsConfig.ships["ExampleFaction"][CustomFactionsConfig.CLASS.MILITARY.FRIGATTE] =
+CustomFactionsConfig.ships["ExampleFaction"][CustomFactionsConfig.CLASS.MILITARY.FRIGATE] =
     { {title = "Example Frigatte A" , plan = "mods/CustomFactions/plans/khillmarr/KhillmarrDaggerCorvette_1.xml", scale = 1.0},
       {title = "Example Frigatte B" , plan = "mods/CustomFactions/plans/khillmarr/KhillmarrDaggerCorvette_1.xml", scale = 1.0},
-      {title = "Example Frigatte C" , plan = "mods/CustomFactions/plans/khillmarr/KhillmarrDaggerCorvette_1.xml", scale = 1.0}
+      {title = "Example Frigatte C" , plan = "mods/CustomFactions/plans/khillmarr/KhilälmarrDaggerCorvette_1.xml", scale = 1.0}
   }
 
 -- Custom events, currently only attacks with Faction and then shipcompositon
@@ -47,27 +47,27 @@ CustomFactionsConfig.customEvents = {
     minimum = 5 * 60, from = 0, to = 500},
   {schedule = random():getInt(35, 70) * 60, script = "mods/CustomFactions/scripts/player/customAttack",
     arguments = {"Khillmarr", {CustomFactionsConfig.CLASS.MILITARY.SENTINEL,
-                               CustomFactionsConfig.CLASS.MILITARY.FRIGATTE,
-                               CustomFactionsConfig.CLASS.MILITARY.FRIGATTE,
+                               CustomFactionsConfig.CLASS.MILITARY.FRIGATE,
+                               CustomFactionsConfig.CLASS.MILITARY.FRIGATE,
                                CustomFactionsConfig.CLASS.MILITARY.SENTINEL}},
     minimum = 25 * 60, to = 500},
   {schedule = random():getInt(60, 80) * 60, script = "mods/CustomFactions/scripts/player/customAttack",
    arguments = {"Khillmarr", {CustomFactionsConfig.CLASS.MILITARY.SENTINEL,
                               CustomFactionsConfig.CLASS.MILITARY.HUNTER,
-                              CustomFactionsConfig.CLASS.MILITARY.FRIGATTE,
+                              CustomFactionsConfig.CLASS.MILITARY.FRIGATE,
                               CustomFactionsConfig.CLASS.MILITARY.CRUISER,
-                              CustomFactionsConfig.CLASS.MILITARY.FRIGATTE,
+                              CustomFactionsConfig.CLASS.MILITARY.FRIGATE,
                               CustomFactionsConfig.CLASS.MILITARY.HUNTER,
                               CustomFactionsConfig.CLASS.MILITARY.SENTINEL}},
    minimum = 60 * 60, to = 350},
   {schedule = random():getInt(80, 120) * 60, script = "mods/CustomFactions/scripts/player/customAttack",
   arguments = {"Khillmarr", {CustomFactionsConfig.CLASS.MILITARY.SENTINEL,
                              CustomFactionsConfig.CLASS.MILITARY.HUNTER,
-                             CustomFactionsConfig.CLASS.MILITARY.FRIGATTE,
+                             CustomFactionsConfig.CLASS.MILITARY.FRIGATE,
                              CustomFactionsConfig.CLASS.MILITARY.CRUISER,
                              CustomFactionsConfig.CLASS.MILITARY.DESTROYER,
                              CustomFactionsConfig.CLASS.MILITARY.CRUISER,
-                             CustomFactionsConfig.CLASS.MILITARY.FRIGATTE,
+                             CustomFactionsConfig.CLASS.MILITARY.FRIGATE,
                              CustomFactionsConfig.CLASS.MILITARY.HUNTER,
                              CustomFactionsConfig.CLASS.MILITARY.SENTINEL}},
    minimum = 120 * 60, to = 300},
