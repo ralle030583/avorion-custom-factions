a52533ffadfed10c5636ddd5c35b65799389d47a
Ralle030583 <ralle030583@users.noreply.github.com>
2017-10-10 20:12:16 +0200

    another copy&paste error...

--- mods/CustomFactions/config/CustomFactionsConfig.lua
+++ mods/CustomFactions/config/CustomFactionsConfig.lua
@@ -4,4 +4,4 @@ CustomFactionsConfig.version = "[0.0.3]"
 CustomFactionsConfig.ModName = "[CustomFactions]"
 CustomFactionsConfig.pauseTime = 10 -- Pausetime after events in minutes
 
-return MoveUIConfig
+return CustomFactionsConfig
