191414df321029e283659c907b6839153e41b67e
Ralle030583 <ralle030583@users.noreply.github.com>
2017-10-10 19:31:13 +0200

    checking if Config exsists

--- mods/CustomFactions/scripts/player/extend_eventscheduler.lua
+++ mods/CustomFactions/scripts/player/extend_eventscheduler.lua
@@ -15,18 +15,20 @@ exsist, CustomFactionsConfig = pcall(require, 'CustomFactionsConfig')
 EventScheduler = {}
 
 function EventSheduler.addCustomEvents(events)
-
-  -- TODO READ FROM CONFIG
-  events.insert({schedule = random():getInt(25, 50) * 60, script = "khillmarrattack", arguments = {0}, minimum = 5 * 60, from = 0, to = 500})
-  events.insert({schedule = random():getInt(35, 70) * 60, script = "khillmarrattack", arguments = {1}, minimum = 25 * 60, to = 500})
-  events.insert({schedule = random():getInt(60, 80) * 60, script = "khillmarrattack", arguments = {2}, minimum = 60 * 60, to = 350})
-  events.insert({schedule = random():getInt(80, 120) * 60, script = "khillmarrattack", arguments = {3}, minimum = 120 * 60, to = 300})
-
+  if exsists then
+    -- TODO READ FROM CONFIG
+    events.insert({schedule = random():getInt(25, 50) * 60, script = "khillmarrattack", arguments = {0}, minimum = 5 * 60, from = 0, to = 500})
+    events.insert({schedule = random():getInt(35, 70) * 60, script = "khillmarrattack", arguments = {1}, minimum = 25 * 60, to = 500})
+    events.insert({schedule = random():getInt(60, 80) * 60, script = "khillmarrattack", arguments = {2}, minimum = 60 * 60, to = 350})
+    events.insert({schedule = random():getInt(80, 120) * 60, script = "khillmarrattack", arguments = {3}, minimum = 120 * 60, to = 300})
+  end
   return events
 end
 
 function EventSheduler.getCustomPauseTime()
-  return CustomFactionsConfig.pauseTime
-end
-
+  if exsist then
+    return CustomFactionsConfig.pauseTime
+  else
+    return 5 -- default vanilla setting
+  end
 end
